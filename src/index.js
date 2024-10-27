71eed4e7876ceadf1694f25ef54c226386f922ee
LeanaRain <137782467+LeanaRain@users.noreply.github.com>
2024-10-27 15:23:31 +0500

    feat: add code

--- src/index.js
+++ src/index.js
@@ -38,7 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    return expr.split(' ').map()
+    let sentense = '';
+    //строка разбив. на блоки по 10 символов
+    for (let i = 0; i < expr.length; i += 10) {
+        const morseChar = expr.slice(i, i + 10);
+        
+        // Если символы 0 (пробел) - пропускаем 
+        if (morseChar === '**********') {
+            sentense += ' ';
+            continue;
+        }
+
+        let morseSymbol = '';
+
+        //10 замен. на точку, 11 на тире
+        for (let j = 0; j < morseChar.length; j += 2) {
+            const pair = morseChar.slice(j, j + 2);
+            if (pair === '10') {
+                morseSymbol += '.';
+            } else if (pair === '11') {
+                morseSymbol += '-';
+            }
+            // Если 00 пропуск
+        }
+
+        //const morseSymbol = morseChar.replace(/10/g, '.').replace(/11/g, '-').replace(/0/g, '');
+
+        sentense += MORSE_TABLE[morseSymbol] || '';
+    }
+    return sentense;
 }
 
 module.exports = {
